4281fc6eddde643413f009573fa95213accd8a0f
Brettly <brettcd@icloud.com>
2022-02-05 22:38:45 -0800

    Build create table, insert data, and delete data functions

--- dice_db.py
+++ dice_db.py
@@ -5,3 +5,37 @@ import matplotlib.pyplot as plt #importing matplotlib below to show distribution
 import matplotlib.dates as mdates
 from matplotlib import style
 style.use('fivethirtyeight')
+
+conn = sqlite3.connect('dice.db')
+c = conn.cursor()
+
+def create_table(): 
+    c.execute('CREATE TABLE IF NOT EXISTS rollHistory(rollID INTEGER PRIMARY KEY, datestamp TEXT, gameTitle TEXT, gameID TEXT, diceCount INTEGER, value INTEGER)')
+
+    c.close()
+    conn.close()
+
+def data_entry(game, gameID, dice, roll):
+    timevalue = time.time()
+    date = str(datetime.datetime.fromtimestamp(timevalue).strftime('%Y-%m-%d %H:%M:%S'))
+
+    conn = sqlite3.connect('dice.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO rollHistory (datestamp, gameTitle, gameID, diceCount, value) VALUES (?, ?, ?, ?, ?)", (date, game, gameID, dice, roll))
+
+    conn.commit()
+
+    c.close()
+    conn.close()
+
+def data_delete():
+    c.execute("DELETE FROM rollHistory WHERE ")
+    conn.commit()
+
+    c.close()
+    conn.close()
+
+
+
+
