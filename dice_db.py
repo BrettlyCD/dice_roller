79e5512961035e295d38a5102f45c1d6ec51cbcf
Brettly <brettcd@icloud.com>
2022-02-05 18:49:16 -0800

    Create python template which will be used for SQLite

---
+++ dice_db.py
@@ -0,0 +1,7 @@
+import sqlite3
+import time
+import datetime
+import matplotlib.pyplot as plt #importing matplotlib below to show distribution of dice rolls
+import matplotlib.dates as mdates
+from matplotlib import style
+style.use('fivethirtyeight')
